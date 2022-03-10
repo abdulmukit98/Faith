2a2e9093f37065b122fd325c7f8956a0fe0e6a36
abdulmukit98 <mdabdulmukit98@gmail.com>
2022-03-10 21:25:54 +0600

    white noise

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import speech_recognition as recognito
+import speech_recognition as sr
 import pyttsx3
 
 Faith = pyttsx3.init()
 Faith.say("Welcome to Bliss")
 Faith.runAndWait()
 
-listener = recognito.Recognizer()
+listener = sr.Recognizer()
+
 try:
-    with recognito.Microphone() as mic:
+    with sr.Microphone() as mic:
+        listener.adjust_for_ambient_noise(mic)
+
         print("listening")
         voice = listener.listen(mic)
+        print("complete")
         speech = listener.recognize_google(voice)
         speech = speech.lower()
         print(speech)
